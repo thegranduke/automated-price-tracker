1160b33906d49ad5373908f0e94e37eb58c54326
Eino <einompi@gmail.com>
2024-12-16 09:55:27 +0200

    Add a feature to check if Product URLs are valid

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils import is_valid_url
 
 # Set up sidebar
 
@@ -6,6 +7,14 @@ with st.sidebar:
     st.title('Add New Product')
     product_url = st.text_input('Product URL')
     add_button = st.button('Add Product')
+
+    if add_button:
+        if not product_url:
+            st.error("Please enter a product URL")
+        elif not is_valid_url(product_url):
+            st.error("Please enter a valid URL")
+        else:
+            st.success("Product is now being tracked!")
 
 # Main content
 st.title("Price Tracker Dashboard")
